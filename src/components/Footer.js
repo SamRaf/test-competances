4c80546014d8e7a50b9a68c934f484018a7d4fc1
SamRaf <samrafrafi93@gmail.com>
2020-09-28 20:42:40 +0200

    creation du footer avec le debut de css pour qu'il soit fixe en bas de page

---
+++ src/components/Footer.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "../styles/styles.css";
+
+function Footer() {
+  return (
+    <div className="center">
+      <p className="footer">
+        Pour vider l'intégralitée de votre panier cliquez ici !!!
+      </p>
+    </div>
+  );
+}
+export default Footer;
