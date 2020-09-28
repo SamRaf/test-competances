320b39d6c198f6d32c5f223799034d4fa9159e9c
SamRaf <samrafrafi93@gmail.com>
2020-09-28 18:37:13 +0200

    creation des produits pour le scroll horizontal

---
+++ src/components/Cards.js
@@ -0,0 +1,103 @@
+import React from "react";
+
+function Cards() {
+  const tab = [
+    {
+      id: "1",
+      title: "Renault",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam at quasi asperiores excepturi distinctio, veniam delectus, saepe libero aut ut rem vel, ipsam ullam optio! Aut sit a quidem magnam.",
+    },
+    {
+      id: "2",
+      title: "Peageot",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam at quasi asperiores excepturi distinctio, veniam delectus, saepe libero aut ut rem vel, ipsam ullam optio! Aut sit a quidem magnam.",
+    },
+    {
+      id: "3",
+      title: "Citroen",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam at quasi asperiores excepturi distinctio, veniam delectus, saepe libero aut ut rem vel, ipsam ullam optio! Aut sit a quidem magnam.",
+    },
+    {
+      id: "4",
+      title: "Dacia",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam at quasi asperiores excepturi distinctio, veniam delectus, saepe libero aut ut rem vel, ipsam ullam optio! Aut sit a quidem magnam.",
+    },
+    {
+      id: "5",
+      title: "Volkswagen",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam at quasi asperiores excepturi distinctio, veniam delectus, saepe libero aut ut rem vel, ipsam ullam optio! Aut sit a quidem magnam.",
+    },
+    {
+      id: "6",
+      title: "Ford",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam at quasi asperiores excepturi distinctio, veniam delectus, saepe libero aut ut rem vel, ipsam ullam optio! Aut sit a quidem magnam.",
+    },
+    {
+      id: "7",
+      title: "Nissan",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam at quasi asperiores excepturi distinctio, veniam delectus, saepe libero aut ut rem vel, ipsam ullam optio! Aut sit a quidem magnam.",
+    },
+    {
+      id: "8",
+      title: "Toyota",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam at quasi asperiores excepturi distinctio, veniam delectus, saepe libero aut ut rem vel, ipsam ullam optio! Aut sit a quidem magnam.",
+    },
+    {
+      id: "9",
+      title: "Opel",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam at quasi asperiores excepturi distinctio, veniam delectus, saepe libero aut ut rem vel, ipsam ullam optio! Aut sit a quidem magnam.",
+    },
+    {
+      id: "10",
+      title: "Audi",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam at quasi asperiores excepturi distinctio, veniam delectus, saepe libero aut ut rem vel, ipsam ullam optio! Aut sit a quidem magnam.",
+    },
+    {
+      id: "11",
+      title: "Mercedes",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam at quasi asperiores excepturi distinctio, veniam delectus, saepe libero aut ut rem vel, ipsam ullam optio! Aut sit a quidem magnam.",
+    },
+    {
+      id: "12",
+      title: "BMW",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam at quasi asperiores excepturi distinctio, veniam delectus, saepe libero aut ut rem vel, ipsam ullam optio! Aut sit a quidem magnam.",
+    },
+    {
+      id: "13",
+      title: "Fiat",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam at quasi asperiores excepturi distinctio, veniam delectus, saepe libero aut ut rem vel, ipsam ullam optio! Aut sit a quidem magnam.",
+    },
+    {
+      id: "14",
+      title: "DS",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam at quasi asperiores excepturi distinctio, veniam delectus, saepe libero aut ut rem vel, ipsam ullam optio! Aut sit a quidem magnam.",
+    },
+    {
+      id: "15",
+      title: "Kia",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam at quasi asperiores excepturi distinctio, veniam delectus, saepe libero aut ut rem vel, ipsam ullam optio! Aut sit a quidem magnam.",
+    },
+  ];
+  return (
+    <>
+      <p>Hello</p>
+    </>
+  );
+}
+
+export default Cards;
